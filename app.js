6cacbbb103afa32f1e09a4f7dc7ef2b50a97931e
Aishu2604 <aishwary.nigam04@gmail.com>
2023-05-15 21:02:52 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const shopRoutes = require("./routes/shop");
 const app = express();
+
+const adminRoutes = require("./routes/admin");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  // console.log("In the middleware!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+  res.status(404).send("<h1>Page Not Found....Please Go back</h1>");
 });
 
 app.listen(3000);
