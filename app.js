a7b32627e7c55030f2b2fdce113a2e0832a8cc61
Aishu2604 <aishwary.nigam04@gmail.com>
2023-05-18 23:46:12 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const shopRoutes = require("./routes/shop");
 const app = express();
-
+const path = require("path");
+const rootDir = require("./util/path");
+const contact = require("./routes/contact");
 const adminRoutes = require("./routes/admin");
+const success = require("./routes/success")
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", contact);
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(success)
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found....Please Go back</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
